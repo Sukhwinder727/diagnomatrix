cf5112bccf653d944bd4f16a7b5bb949b384c069
Sukhwinder727 <sukhwinder290804@gmail.com>
2025-12-14 19:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,6 @@ def predictBreast():
 
 import os
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     app.run(debug=True, host="0.0.0.0", port=port)
